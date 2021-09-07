fd8e42aac515bf18d25d15b01217b2c75dc29f7b
KateKrechet <k.krechet@yandex.ru>
2021-09-07 22:33:55 +0300

    Homework

--- Dynamic_Arrays/Dynamic_Arrays.cpp
+++ Dynamic_Arrays/Dynamic_Arrays.cpp
@@ -1,58 +1,57 @@
 ﻿#include <iostream>
 using namespace std;
 
+void Print(int arr[], const unsigned int n);
+
 void main()
 {
 	setlocale(LC_ALL, "Russian");
 	int n;
 	cout << "Введите размер массива: "; cin >> n;
 	int* arr = new int[n] {};
-	/*int* brr = new int[count_even] {};
-	int* crr = new int[count_odd] {};*/
 	int count_even = 0, count_odd = 0;
 
 	for (int i = 0; i < n; i++)
 	{
 		arr[i] = rand() % 100;
+		(arr[i] % 2 == 0) ? count_even++ : count_odd++;
+
+	}
+	Print(arr, n);
+	cout << "Количество четных элементов массива " << count_even << endl;
+	cout << "Количество нечетных элементов массива " << count_odd << endl;
+	int* brr = new int[count_even] {};
+	int* crr = new int[count_odd] {};
+	count_even = 0;
+	count_odd = 0;
+	for (int i = 0; i < n; i++)
+	{
 		if (arr[i] % 2 == 0)
 		{
-			count_even ++;
-			/*for (int j = 0; j < count_even; j++)
-			{
-				brr[j] = arr[i];
-			}*/
+			brr[count_even] = arr[i];
+			count_even++;
 		}
+
 		else
 		{
+			crr[count_odd] = arr[i];
 			count_odd++;
-			/*for (int k = 0; k < count_odd; k++)
-			{
-				crr[k] = arr[i];
-			}*/
 		}
 	}
-
-
+	delete[] arr;
+	cout << "Вывод четных элементов массива" << endl;
+	Print(brr, count_even);
+	delete[] brr;
+	cout << "Вывод нечетных элементов массива" << endl;
+	Print(crr, count_odd);
+	delete[] crr;
+}
+void Print(int arr[], const unsigned int n)
+{
 	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << "\t";
 	}
 	cout << endl;
-	delete[] arr;
-	/*for (int j = 0; j < count_even; j++)
-	{
-		cout << brr[j] << "\t";
-	}
-	cout << endl;
-	delete[] brr;
-	for (int k = 0; k < count_odd; k++)
-	{
-		cout << crr[k] << "\t";
-	}
-	cout << endl;
-	delete[] crr;*/
-	cout << "Количество четных элементов массива " << count_even << endl;
-	cout << "Количество нечетных элементов массива " << count_odd << endl;
-
 }
 
